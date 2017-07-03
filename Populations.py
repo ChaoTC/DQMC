69a19471c8d9bbba91a61fc48cffd10930b7a07b
Jules Randolph <julran13@gmail.com>
2017-07-03 17:35:56 -0600

    ACTUALLY fixed merge conflict

--- Populations.py
+++ Populations.py
@@ -37,7 +37,6 @@ class Population2D(object):
 class Population3D(object):
 
 	@staticmethod
-<<<<<<< HEAD
 	def initPopulation(size,xSteps,ySteps,xSteps,xLength,yLength,zLength):
 		xStep=xLength/xSteps
 		yStep=yLength/ySteps
@@ -54,25 +53,6 @@ class Population3D(object):
 						population.append(Psip3D(xCoordinate,yCoordinate,zCoordinate))
 		return population
 
-
-=======
-	def initPopulation(size, xSteps, ySteps, zSteps, xLength, yLength, zLength):
-		xStep = xLength/xSteps
-		yStep = yLength/ySteps
-		zStep = zLength/zSteps
-		population = []
-		for x in range(xSteps):
-			for y in range(ySteps):
-				for z in range (zSteps):
-					xCoordinate = (x * xStep) - (xLength/2)
-					yCoordinate = (y * yStep) - (yLength/2)
-					zCoordinate = (z * yStep) - (zLength/2)
-					copiesAtCoordinate = size/(xSteps * ySteps * zStep)
-					for _ in range(copiesAtCoordinate):
-						population.append(Psip2D(xCoordinate, yCoordinate, zCoordinate))
-		return population
-
->>>>>>> 616803b1fec8813072fd755859e992955ef8d9d9
 	@staticmethod
 	def printPopulation(population):
 		for member in population:
